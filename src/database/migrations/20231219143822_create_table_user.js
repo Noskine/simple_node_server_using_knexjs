4ed749b314b3edcff97cb15d0aab131950e81380
Noskine <noskine@outlook.com>
2023-12-19 14:13:17 -0300

    update: defaultTo uuid

--- src/database/migrations/20231219143822_create_table_user.js
+++ src/database/migrations/20231219143822_create_table_user.js
@@ -4,7 +4,7 @@
  */
 export const up = async (knex) => {
   await knex.schema.createTable('users', (table) => {
-    table.uuid('id').notNullable().primary()
+    table.uuid('id').notNullable().primary().defaultTo(knex.fn.uuid())
     table.string('username', 80).notNullable()
     table.string('email', 80).notNullable().unique()
     table.string('password_hash').notNullable()
